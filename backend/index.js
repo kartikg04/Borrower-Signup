3da9e19077cb5e7b960fe70294a68f4423660d79
Kartik Gupta <kg85303@gmail.com>
2025-06-07 17:19:55 +0530

    Add root route and update backend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-app.get("/", (req, res) => {
-  res.send("Backend is working!");
-});
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
